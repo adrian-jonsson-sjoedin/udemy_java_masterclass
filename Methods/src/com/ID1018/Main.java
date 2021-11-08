9f9bb5a5eb8f4058d9d52bba881f777d8c22e9c2
adriansj <adriansj@kth.se>
2021-11-08 17:55:21 +0100

    Update Main.java

--- Methods/src/com/ID1018/Main.java
+++ Methods/src/com/ID1018/Main.java
@@ -11,13 +11,29 @@ public class Main {
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
-        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score is " + highScore);
 
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score is " + highScore);
+
+        int highScorePosition = calculatedHighScorePosition(calculateScore(true, 500, 5, 100)); //score=3000
+        displayHighScorePosition("Adrian", highScorePosition);
+
+        highScorePosition = calculatedHighScorePosition(800);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculatedHighScorePosition(100);
+        displayHighScorePosition("Elias", highScorePosition);
+
+        highScorePosition = calculatedHighScorePosition(50);
+        displayHighScorePosition("Teo", highScorePosition);
+
     }
 
     //this is a new method that I've created. it is kinda the same thing as a function in python
@@ -29,9 +45,25 @@ public class Main {
         if (statusOfGame) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score is " + finalScore);
             return finalScore;
         }
         return -1; //-1 is used to indicate an error or in searching algorithms an invalid value or a value not found
+    }
+
+    // Takes the players high score and assign it a position
+    public static int calculatedHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if ((score >= 500) && (score < 1000)) { //can remove && (score < 1000) since we've already checked it above
+            return 2;
+        } else if ((score >= 100) && (score < 500)) { //can remove && (score < 500) since we've already checked it above
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " got position " + position + " on the high score table");
     }
 }
