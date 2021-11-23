61a3da405165e1a1e578c03f25d56c385ed8e4b2
adriansj <adriansj@kth.se>
2021-11-23 12:08:39 +0100

    section 6

--- ConstructorChallenge/src/com/Adrian/Main.java
+++ ConstructorChallenge/src/com/Adrian/Main.java
@@ -3,9 +3,15 @@ package com.Adrian;
 public class Main {
 
     public static void main(String[] args) {
-	VipCustomer adrian = new VipCustomer();
+        VipCustomer adrian = new VipCustomer();
         System.out.println(adrian.getCustomerName());
         System.out.println(adrian.getCreditLimit());
         System.out.println(adrian.getEmail());
+
+        VipCustomer person2 = new VipCustomer("Bob", "25000");
+        System.out.println(person2.getCustomerName());
+        System.out.println(person2.getCreditLimit());
+        System.out.println(person2.getEmail());
+
     }
 }
