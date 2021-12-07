d240c155999e5b3e75b5258cfad66d625c2b0a14
adriansj <adriansj@kth.se>
2021-12-07 09:37:58 +0100

    section 8

---
+++ MimimumElementChallenge/src/com/Adrian/Main.java
@@ -0,0 +1,24 @@
+package com.Adrian;
+
+import java.util.Scanner;
+
+public class Main {
+private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+
+    private static int readInteger() {
+        System.out.println("Enter how many elements you want your array to be:");
+        int count = scanner.nextInt();
+        return count;
+    }
+
+    private static int[] readElements(int count) {
+        for (int i =0;i<count;i++)
+    }
+
+    private static int findMin(int[] array) {
+
+    }
+}
--- ReferenceVsValueType/src/com/Adrian/Main.java
+++ ReferenceVsValueType/src/com/Adrian/Main.java
@@ -20,23 +20,28 @@ public class Main {
         //object but not the object themselves
         int[] anotherArray = myIntArray;//this points to the same object as myIntArray. has the same adress
 
-        System.out.println("myIntArray = "+ Arrays.toString(myIntArray));
-        System.out.println("anotherArray ="+Arrays.toString(anotherArray));
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray =" + Arrays.toString(anotherArray));
 
         anotherArray[0] = 1;//this will change the value of the integer on index 0
         //for the object anotherArray points to. Meaning this also
         //changes myIntArray
-        System.out.println("after change myIntArray = "+Arrays.toString(myIntArray));
-        System.out.println("after change anotherArray = "+Arrays.toString(anotherArray));
+        System.out.println("after change myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray = " + Arrays.toString(anotherArray));
 
-        modifyArray(myIntArray);
+       anotherArray=new int[]{4,5,6,7,8};//creates a new object in memory with a new address
+        modifyArray(myIntArray);//changes the object the array is referencing and thus all references
+        //pointing to that object will have the same changed output
 
-        System.out.println("after modifyArray myIntArray = "+Arrays.toString(myIntArray));
-        System.out.println("after modifyArray anotherArray = "+Arrays.toString(anotherArray));
+        System.out.println("after modifyArray myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after modifyArray anotherArray = " + Arrays.toString(anotherArray));
+
 
     }
 
     private static void modifyArray(int[] array) {
-        array[0] =2;
+        array[0] = 2;
+        array = new int[]{1, 2, 3, 4, 5};
+
     }
 }
