4510c8426c7d7b349a5000b4e81b020d8d6a94a1
Adrian Jonsson Sjödin <adrian.jonsson@outlook.com>
2021-12-26 16:31:21 +0100

    code challenges from section 5

---
+++ PaintJobChallenge/src/PaintJob.java
@@ -0,0 +1,29 @@
+public class PaintJob {
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        double wallArea = width * height;
+        double dBuckets = wallArea / areaPerBucket;
+        int buckets = (int) Math.ceil(dBuckets);
+        return buckets - extraBuckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double wallArea = width * height;
+        double dBuckets = wallArea / areaPerBucket;
+        int buckets = (int) Math.ceil(dBuckets);
+        return buckets;
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+}
