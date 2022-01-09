e8bdbdcbe014e4735d5599cd52ef402351187209
adriansj <adriansj@kth.se>
2022-01-09 19:37:42 +0100

    Interfaces first part. section 9

---
+++ Interface/src/com/Adrian/ITelephone.java
@@ -0,0 +1,15 @@
+package com.Adrian;
+
+public interface ITelephone {
+
+    void powerOn();
+
+    void dial(int phoneNumber);
+
+    void answer();
+
+    boolean callPhone(int phoneNumber);
+
+    boolean isRinging();
+
+}
